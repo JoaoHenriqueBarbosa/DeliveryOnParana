315dc87bd5fc62429d22767eb2934d39a80d1af2
João Henrique Barbosa <joaohenriquebarbosa21@gmail.com>
2022-06-03 19:11:40 -0300

    feat: get user input and use physics

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+
+    [SerializeField] float steerSpeed = 300f;
+    [SerializeField] float moveSpeed = 10f;
+
     void Start()
     {
 
@@ -11,7 +15,9 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.01f, 0);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
